7aea2480b920371ae0e9502add5faaf7f9a8c9cb
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-05-22 10:56:34 +0200

    fix: use example base model in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
 import { Tree } from "./components/Tree.tsx";
 import { TreeContext, createTreeManagerFromModelNodes } from "./contexts/TreeContext.ts"
 import { ModelDefinition, getModelNodes } from "./modules/LayeredModel.ts"
+import { baseModel } from "../examples/example-models.ts"
 
 import "@xyflow/react/dist/style.css";
 import "./App.css";
 
 const App = () => {
-  const modelDefintion: ModelDefinition = {
-    nodes: {
-      "root-item" : {
-        type: "essential-element",
-        name: {
-            shortName: "Root",
-            longName: "Root Item"
-        },
-        description: "This is a Root item.",
-        considerationLevel: "core",
-      },
-      "child-item-a" : {
-        type: "category",
-        parentId: "root-item",
-        name: {
-            shortName: "Child A",
-            longName: "Child Item A"
-        },
-        description: "This is child item A",
-        considerationLevel: "core",
-      }
-    }
-  }
+  const modelDefintion: ModelDefinition = baseModel;
   const nodes = getModelNodes(modelDefintion);
   const treeManager = createTreeManagerFromModelNodes(nodes);
   return (
