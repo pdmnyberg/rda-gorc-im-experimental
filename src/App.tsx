0dfad153287c65d56b28869eab0476f941b25e61
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-06-02 16:43:05 +0200

    feat: add support for slecting and applying thematic slices

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,40 @@ import { NavLink } from "react-router";
 import React from "react";
 import { Tree } from "./components/Tree.tsx";
 import { TreeContext, createTreeManagerFromModelNodes, getLayout } from "./contexts/TreeContext.ts"
-import { ModelDefinition, getModelNodes, applyLayers } from "./modules/LayeredModel.ts"
-import { baseModel, profiles } from "../examples/example-models.ts"
+import { ModelDefinition, getModelNodes, applyLayersAndSlices, Package } from "./modules/LayeredModel.ts"
+import { baseModel, profiles, slices } from "../examples/example-models.ts"
 import "@xyflow/react/dist/style.css";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import MultiSelect, {SelectItem} from "./components/MultiSelect/MultiSelect";
 
+
+function usePackageSelect<T extends Package>(packages: T[] | Record<string, T>): [T[], SelectItem[], string[], (pkgs: string[]) => void] {
+  const [packageIds, setPackageIds] = React.useState<string[]>([]);
+  const selectedPackages = React.useMemo(() => Object.values(packages).filter(p => packageIds.includes(p.id)), [packages, packageIds]);
+  return [
+    selectedPackages,
+    Object.values(packages).map(p => ({
+      id: p.id,
+      label: p.label,
+      info: p.version
+    })),
+    packageIds,
+    setPackageIds
+  ]
+}
+
 const App = () => {
-  const [profileIds, setProfileIds] = React.useState<string[]>([]);
-  const selectedProfiles = React.useMemo(() => Object.values(profiles).filter(p => profileIds.includes(p.id)), [profiles, profileIds]);
-  const modelDefintion: ModelDefinition = React.useMemo(() => applyLayers(baseModel, selectedProfiles), [baseModel, selectedProfiles]);
+  const [selectedProfiles, profileItems, profileIds, setProfileIds] = usePackageSelect(profiles);
+  const [selectedSlices, sliceItems, sliceIds, setSliceIds] = usePackageSelect(slices);
+  const modelDefintion: ModelDefinition = React.useMemo(
+    () => applyLayersAndSlices(baseModel, selectedProfiles, selectedSlices),
+    [baseModel, selectedProfiles, selectedSlices]
+  );
   const nodes = React.useMemo(() => getModelNodes(modelDefintion), [modelDefintion]);
   const nodeSize = 120;
   const layout = React.useMemo(() => getLayout(nodes, nodeSize), [nodes, nodeSize]);
   const treeManager = createTreeManagerFromModelNodes(nodes, layout);
-  const profileItems: SelectItem[] = Object.values(profiles).map(p => ({
-    id: p.id,
-      label: p.label,
-      info: p.version
-  }));
   return (
     <>
       <TreeContext.Provider value={treeManager}>
@@ -31,6 +45,8 @@ const App = () => {
               component: <>
                 <h2>Select profiles</h2>
                 <MultiSelect items={profileItems} selection={profileIds} onChange={setProfileIds}/>
+                <h2>Select slices</h2>
+                <MultiSelect items={sliceItems} selection={sliceIds} onChange={setSliceIds}/>
               </>,
               icon: <></>
             },
