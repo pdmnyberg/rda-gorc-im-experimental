313017b8cc34a1f9083f184d7b5e4062db1e7c76
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2025-06-04 13:28:29 +0200

    fix: after merging conflicts and refactoring of Header and Layout component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import { NavLink } from "react-router";
 import './Header.css';
 
-function Header() {
+type PanelButton = {
+    id: string;
+    icon: React.ReactNode;
+    onClick: () => void;
+};
+
+type HeaderProps = {
+    panelButtons?: PanelButton[];
+};
+
+function Header({ panelButtons = [] }: HeaderProps) {
     return (
         <header className="header">
             <div className="header-title">RDA Visualisation App</div>
@@ -10,9 +20,15 @@ function Header() {
                 <ul className="nav-list">
                     <li><NavLink to="/documentation">Documentation</NavLink></li>
                 </ul>
+                {panelButtons?.map(btn => (
+                    <div key={btn.id} className="panel-button" onClick={btn.onClick}>
+                        {btn.icon}
+                    </div>
+                ))}
             </nav>
         </header>
     );
 }
+
 
 export default Header;
