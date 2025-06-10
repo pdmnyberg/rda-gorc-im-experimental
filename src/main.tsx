82358972f0b4df76f386967b38d6499e7197b814
Henrike <henrike.wiemker@t-online.de>
2025-06-10 10:55:38 +0200

    feat: add Documentation folder and Documentation.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
-import { Documentation } from "./pages/Documentation.tsx";
+import { Documentation } from "./pages/Documentation/Documentation.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
