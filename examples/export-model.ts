7fee759ba61e16ac2721c422efe0084166b02f90
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-06-10 11:32:37 +0200

    feat: load repositories from app config

--- examples/export-model.ts
+++ examples/export-model.ts
@@ -1,12 +1,25 @@
 import {models, profiles, slices} from "./example-models";
 import {Package} from "../src/modules/LayeredModel";
+import {RepositoryRoot} from "../src/modules/RepositorySource";
 import fs from "node:fs";
 
 function main() {
-    const rootPath = "dist-examples";
+    const repoId = "example-repo";
+    const rootPath = `public/${repoId}`;
     exportPackages(`${rootPath}/models`, models);
     exportPackages(`${rootPath}/profiles`, profiles);
     exportPackages(`${rootPath}/slices`, slices);
+    const repoRoot: RepositoryRoot = {
+        id: repoId,
+        name: "Example Repo",
+        description: "This is an example repo",
+        url: `${repoId}/root.json`,
+        baseModels: Object.values(models).map(m => ({ref: `${repoId}/models/${m.id}.json`})),
+        profiles: Object.values(profiles).map(p => ({ref: `${repoId}/profiles/${p.id}.json`, modelId: p.modelId})),
+        thematicSlices: Object.values(slices).map(s => ({ref: `${repoId}/slices/${s.id}.json`, modelId: s.modelId})),
+    }
+    fs.mkdirSync(rootPath, {recursive: true});
+    fs.writeFileSync(`${rootPath}/root.json`, JSON.stringify(repoRoot));
 }
 
 function exportPackages<T extends Package>(rootPath: string, packages: {[x: string]: T}) {
