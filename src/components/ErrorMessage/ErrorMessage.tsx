8028a25037d283a384f84498c95553004c144e52
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2025-08-18 15:37:03 +0200

    feat: improve UI for error message

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -2,13 +2,13 @@ import "./ErrorMessage.css";
 
 export const ErrorMessage = ({ message }: { message: string }) => {
   return (
-    <div className="error-container">
-      <p>Oh no!</p>
-      <p>
-        We couldn't fetch data from the chosen repository. <br></br> Please
-        choose a different one.
-      </p>
-      <pre>{message}</pre>
+    <div className="error-content">
+        <h2>Oh no!</h2>
+        <p>
+            We couldn't fetch data from the chosen repository. <br></br> Please
+            choose a different one.
+        </p>
+        <pre>{message}</pre>
     </div>
   );
 };
