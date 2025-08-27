1cf757bf515465ae72da5dd5647bc627ac098aa3
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-08-27 14:01:33 +0200

    chore: apply prettify

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -3,12 +3,12 @@ import "./ErrorMessage.css";
 export const ErrorMessage = ({ message }: { message: string }) => {
   return (
     <div className="error-content">
-        <h2>Oh no!</h2>
-        <p>
-            We couldn't fetch data from the chosen repository. <br></br> Please
-            choose a different one.
-        </p>
-        <pre>{message}</pre>
+      <h2>Oh no!</h2>
+      <p>
+        We couldn't fetch data from the chosen repository. <br></br> Please
+        choose a different one.
+      </p>
+      <pre>{message}</pre>
     </div>
   );
 };
