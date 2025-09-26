b56a6bfcf86962f1d4f4493747e4fc2f1e5658a2
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-09-26 15:33:19 +0200

    fix: clean up to make system build

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Home.css";
 import { Tree } from "../../components/Tree/Tree";
 import Layout from "./../../components/Layout/Layout";
@@ -8,7 +8,6 @@ import {
   TreeContext,
   createTreeManagerFromModelNodes,
   getD3Layout,
-  TreeLayout,
 } from "../../contexts/TreeContext";
 import {
   ModelDefinition,
