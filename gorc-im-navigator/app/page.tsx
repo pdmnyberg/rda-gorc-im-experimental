232b0ce27ddd51c580c700db7ef20468f849e5a8
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2026-05-27 10:05:10 +0200

    fix: add no model selected indicator

--- gorc-im-navigator/app/page.tsx
+++ gorc-im-navigator/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { Tree } from "@/components/Tree";
-import { Panels } from "@/components/Panels";
 
 export default function Home() {
   return (
--- gorc-im-navigator/components/Tree.tsx
+++ gorc-im-navigator/components/Tree.tsx
@@ -8,10 +8,12 @@ import {
 } from "@xyflow/react";
 import { useTreeContext } from "../contexts/TreeContext";
 import { GORCNodeView } from "./GORCNodeView/GORCNodeView";
+import gorcBgLogo from '../img/gorc-im-icon-black.svg'
 
 import "@xyflow/react/dist/style.css";
 import { GORCNode } from "../modules/GORCNodes";
 import { useNodeSelection } from "../contexts/SelectionContexts";
+import Image from "next/image";
 
 const nodeTypes = { gorc: GORCNodeView };
 
@@ -31,18 +33,32 @@ export const Tree = () => {
   }, [setSelectedNode]);
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodeClick={onNodeClick}
-      fitView
-      nodeTypes={nodeTypes}
-      nodesDraggable={false}
-      onNodesChange={onNodesChange}
-      minZoom={0.1}
-    >
-      <MiniMap nodeStrokeWidth={3} />
-      <Controls />
-    </ReactFlow>
+    <>
+      {nodes.length > 0 ? (
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodeClick={onNodeClick}
+          fitView
+          nodeTypes={nodeTypes}
+          nodesDraggable={false}
+          onNodesChange={onNodesChange}
+          minZoom={0.1}
+        >
+          <MiniMap nodeStrokeWidth={3} />
+          <Controls />
+        </ReactFlow>
+      ) : (
+        <div className="align-middle h-100 w-100 d-flex justify-content-center align-items-center opacity-25 overflow-hidden flex-column">
+          <Image
+            src={gorcBgLogo.src}
+            alt={""}
+            width={gorcBgLogo.width * 3}
+            height={gorcBgLogo.height * 3}
+          />
+          <div className="m-2">No model selected.</div>
+        </div>
+      )}
+    </>
   );
 };
